591a45c293d4de0e3cdb944c0fb30999efa346aa
y.ack <12588017+y-ack@users.noreply.github.com>
2021-11-30 21:30:08 -0800

    2021 day 1 (powershell)

---
+++ 2021/day1.ps1
@@ -0,0 +1,24 @@
+$in = $in -split "`n"
+function part1([int]$x = 0,[int]$y = 0) {
+  $in | % {$s = 0; $p = 0} {
+    if ($p -ne 0) {
+      if (([int]$_) -gt $p) {$s++}
+    }
+    $p = [int]$_
+  }{$s}
+} part1
+
+function part2([int]$x = 0,[int]$y = 0) {
+  $s=0; $p=0
+  # forgot to cast to int[] (sob)
+  [int[]]$in = [int[]]$in
+  for ($y=2; $y -lt $in.Length; $y++) {
+    if($p -ne 0){
+      $a = $in[$x] + $in[($x+1)] + $in[($x+2)]
+      if ($a -gt $p) { $s++ }
+    }
+    $p = $in[$x] + $in[($x+1)] + $in[($x+2)]
+    $x++
+  }
+  $s
+} day1
